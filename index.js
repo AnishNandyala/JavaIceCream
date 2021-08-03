46a64fd8b31ed44524d366c606b1ccb2c746e9b7
Anish Nandyala <anishnandyala@anishs-air.myfiosgateway.com>
2021-08-03 16:31:30 -0400

    fixed js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-document.getElementById("#place-order").onclick = function () {
-    location.href = "orderpage.html"
-}
+// document.getElementById("place-order").addEventListener("click",function () {
+//     console.log("Works");
+//     location.href = "orderpage.html";
+// })
+var placeOrder = document.getElementById("place-order");
+console.log(placeOrder);
