30712f8cc355405ceeff8cfb364113a41097b3e4
Anish Nandyala <anishnandyala@anishs-air.myfiosgateway.com>
2021-08-04 16:36:58 -0400

    image js

--- order.js
+++ order.js
@@ -10,8 +10,52 @@ document.getElementById("show-me").addEventListener("click", showPicture, false)
 function showPicture () {
     if (chocolateRB.checked) {
         image.setAttribute("src", chocolateRB.value);
+        if (MandMCheckbox.checked && sprinklesCheckbox.checked && chocSprinklesCheckbox.checked) {
+            //all toppings
+            image.setAttribute("src", "icecreamvariations-chocolate/chocolate_icecream--alltoppings.png");
+        } else if (MandMCheckbox.checked && sprinklesCheckbox.checked) {
+            //m&ms and sprinkles
+            image.setAttribute("src", "icecreamvariations-chocolate/chocolate_icecream--m&m+sprinkles.png");
+        } else if (MandMCheckbox.checked && chocSprinklesCheckbox.checked) {
+            //m&ms and sprinkles
+            image.setAttribute("src", "icecreamvariations-chocolate/chocolate_icecream--m&m+chocsprinkles.png");
+        } else if (sprinklesCheckbox.checked && chocSprinklesCheckbox.checked) {
+            //sprinkles and chocolate sprinkles
+            image.setAttribute("src", "icecreamvariations-chocolate/chocolate_icecream--sprinkles+chocsprinkles.png");
+        } else if (MandMCheckbox.checked) {
+            //m&ms
+            image.setAttribute("src", "icecreamvariations-chocolate/chocolate_icecream--m&m.png");
+        } else if (sprinklesCheckbox.checked) {
+            //sprinkles
+            image.setAttribute("src", "icecreamvariations-chocolate/chocolate_icecream--sprinkles.png");
+        } else if (chocSprinklesCheckbox.checked) {
+            //chocolate sprinkles
+            image.setAttribute("src", "icecreamvariations-chocolate/chocolate_icecream--chocsprinkles.png");
+        }
     } else if (vanillaRB.checked) {
         image.setAttribute("src", vanillaRB.value);
+        if (MandMCheckbox.checked && sprinklesCheckbox.checked && chocSprinklesCheckbox.checked) {
+            //all toppings
+            image.setAttribute("src", "icecreamvariations-vanilla/vanilla_icecream--alltoppings.png");
+        } else if (MandMCheckbox.checked && sprinklesCheckbox.checked) {
+            //m&ms and sprinkles
+            image.setAttribute("src", "icecreamvariations-vanilla/vanilla_icecream--m&m+sprinkles.png");
+        } else if (MandMCheckbox.checked && chocSprinklesCheckbox.checked) {
+            //m&ms and sprinkles
+            image.setAttribute("src", "icecreamvariations-vanilla/vanilla_icecream--m&m+chocsprinkles.png");
+        } else if (sprinklesCheckbox.checked && chocSprinklesCheckbox.checked) {
+            //sprinkles and chocolate sprinkles
+            image.setAttribute("src", "icecreamvariations-vanilla/vanilla_icecream--sprinkles+chocsprinkles.png");
+        } else if (MandMCheckbox.checked) {
+            //m&ms
+            image.setAttribute("src", "icecreamvariations-vanilla/vanilla_icecream--m&m.png");
+        } else if (sprinklesCheckbox.checked) {
+            //sprinkles
+            image.setAttribute("src", "icecreamvariations-vanilla/vanilla_icecream--sprinkles.png");
+        } else if (chocSprinklesCheckbox.checked) {
+            //chocolate sprinkles
+            image.setAttribute("src", "icecreamvariations-vanilla/vanilla_icecream--chocsprinkles.png");
+        }
     } else if (strawberryRB.checked) {
         image.setAttribute("src", strawberryRB.value);
         if (MandMCheckbox.checked && sprinklesCheckbox.checked && chocSprinklesCheckbox.checked) {
@@ -19,7 +63,22 @@ function showPicture () {
             image.setAttribute("src", "icecreamvariations-strawberry/strawberry_icecream--alltoppings.png");
         } else if (MandMCheckbox.checked && sprinklesCheckbox.checked) {
             //m&ms and sprinkles
-            image.setAttribute("src", "strawberry_icecream--m&m+sprinkles.png");
+            image.setAttribute("src", "icecreamvariations-strawberry/strawberry_icecream--m&m+sprinkles.png");
+        } else if (MandMCheckbox.checked && chocSprinklesCheckbox.checked) {
+            //m&ms and sprinkles
+            image.setAttribute("src", "icecreamvariations-strawberry/strawberry_icecream--m&m+chocsprinkles.png");
+        } else if (sprinklesCheckbox.checked && chocSprinklesCheckbox.checked) {
+            //sprinkles and chocolate sprinkles
+            image.setAttribute("src", "icecreamvariations-strawberry/strawberry_icecream--sprinkles+chocsprinkles.png");
+        } else if (MandMCheckbox.checked) {
+            //m&ms
+            image.setAttribute("src", "icecreamvariations-strawberry/strawberry_icecream--m&m.png");
+        } else if (sprinklesCheckbox.checked) {
+            //sprinkles
+            image.setAttribute("src", "icecreamvariations-strawberry/strawberry_icecream--sprinkles.png");
+        } else if (chocSprinklesCheckbox.checked) {
+            //chocolate sprinkles
+            image.setAttribute("src", "icecreamvariations-strawberry/strawberry_icecream--chocsprinkles.png");
         }
     }
 }
